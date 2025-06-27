ba2596d82bc313e2c1b989e9a160fe5d37771144
Valentine <valentine@pop-os.Dlink>
2025-06-27 20:11:06 +0300

    Shopping Assistant buttons functionality fixed

--- frontend/src/components/subpages/report_form/ReportForm.jsx
+++ frontend/src/components/subpages/report_form/ReportForm.jsx
@@ -73,7 +73,7 @@ function ReportForm() {
         <BackwardButton/>
       </Link>
 
-      <h2>🌙 Moon Report Generator</h2>
+      <h2 class="text-3xl font-extrabold text-white sm:text-4xl">🌙 Moon Report Generator</h2>
       <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-400 sm:mt-4">Details: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores, nesciunt itaque. Voluptatibus a ex ipsam dolorum provident maiores unde laboriosam qui, cumque nobis aliquam maxime error nostrum reprehenderit laborum esse!</p>
       <div className="form-output-wrapper">  
         <form id="reportForm" class="space-y-6">
