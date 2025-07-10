904fc61773123d1a6f3ef5a38f04ebdd983dcfd9
Valentine <valentine@pop-os.Dlink>
2025-07-10 23:06:10 +0300

    fix: issue#29 cards UI update

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import logo from '/MoonAI-logo.png';
 import { Link } from 'react-router-dom';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import projectCards from '../../shared/ui/cards';
 
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react";
@@ -36,13 +37,9 @@ const Header = () => {
 
   
   const navRoutes = [
-    {label: 'Moon AI Tools', url: 'moon_ai_tools'},
-    {label: <AnchorLink offset={80} href="#contributors" onClick={() => {
-      setBurgerMenuActive(false);
-    }}>Contributors</AnchorLink>},
-    {label: <AnchorLink offset={80} href="#contact" onClick={() => {
-      setBurgerMenuActive(false);
-    }}>Contacts</AnchorLink>},
+    {label: <AnchorLink offset={80} href='#Our Projects' onClick={() => { setBurgerMenuActive(false) }}>Our Projects</AnchorLink>},
+    {label: <AnchorLink offset={80} href="#contributors" onClick={() => { setBurgerMenuActive(false) }}>Contributors</AnchorLink>},
+    {label: <AnchorLink offset={80} href="#contact" onClick={() => { setBurgerMenuActive(false) }}>Contacts</AnchorLink>},
   ];
   
   return (
@@ -103,12 +100,34 @@ const Header = () => {
       </div>
 
       <header id='header' className="header">
+
         <h1 className="header-title">
           Moon AI
         </h1>
+
         <p className="header-subtitle">
           An open-source organization dedicated to developing AI tools for lunar astronomical calculations.
         </p>
+
+        <h2 id='Our Projects' offset={80} class="text-3xl mt-12 font-extrabold text-white sm:text-4xl">Our Projects</h2>
+        <div className='our-projects-info-wrapper'>
+
+          <div className='our-projects-info-inner'>
+            {projectCards.map((cardObject, index) => (
+              <div key={index} className="card">
+                <img className='card-img' src={cardObject.img} alt='MoonAI project image'/>
+                <h3 className='card-h3'>{cardObject.title}</h3>
+              </div>
+            ))}
+          </div>
+
+          <Link to={'/moon_ai_tools'}>
+            <button class="mt-8 py-3 px-6 border border-transparent rounded-md shadow-sm text-base font-medium text-white ">
+              View all Projects
+            </button>
+          </Link>
+        </div>
+        
       </header>
     </section>
   );
---
+++ frontend/src/shared/ui/cards.js
@@ -0,0 +1,40 @@
+import moon_report_generator from '../../assets/Moon-report-generator.png';
+import moon_data_api from '../../assets/Moon-data-api.png';
+import shopping_assistant from '../../assets/Shopping-assistant.png';
+
+const projectCards = [
+  // tech stack and description needs to be changed when ready
+  { 
+    img: moon_report_generator,
+    id: 1, 
+    title: 'Moon Report Generator', 
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos temporibus excepturi deleniti odit molestiae sit modi aperiam ', url: 'moon_report_generator', 
+    tech_stack: ['Java', 'Python', 'Typescript', 'Grok', 'FastAPI', ] },
+  
+  // tech stack, img and description needs to be changed when ready
+  { 
+    img: moon_data_api,
+    id: 2, 
+    title: 'Moon Data API', 
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos temporibus excepturi deleniti odit molestiae sit modi aperiam', url: 'moon_data_api', 
+    tech_stack: ['Java', 'Python', 'Typescript', 'Grok', 'FastAPI', ] },
+
+  // tech stack, img and description needs to be changed when ready
+  { 
+    img: shopping_assistant,
+    id: 3, 
+    title: 'Shopping Assistant', 
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos temporibus excepturi deleniti odit molestiae sit modi aperiam', url: 'shopping_assistant', 
+    tech_stack: ['Java', 'Python', 'Typescript', 'Grok', 'FastAPI', ] },
+
+  // tech stack, img and description needs to be changed when ready
+  { 
+    img: moon_report_generator,
+    id: 4, 
+    title: 'Another One Assistant', 
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos temporibus excepturi deleniti odit molestiae sit modi aperiam', url: 'another_one_assistant', 
+    tech_stack: ['Java', 'Python', 'Typescript', 'Grok', 'FastAPI', ] },
+ 
+];
+
+export default projectCards;
